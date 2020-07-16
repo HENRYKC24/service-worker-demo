be111a249883935536f8000f422752b802cf7655
HENRY-KC <henrykc24@yahoo.com>
2020-07-16 10:59:28 +0100

    test caching listed pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-    .register('../sw-cached-site.js')
+    .register('../sw-cached-pages.js')
     .then(reg => console.log('Successfully registered service worker'))
     .catch(err => console.log('Error: ' + err))
   })
